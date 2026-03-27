29fb632531fa4e4de354f0be705e2f02ec696b6a
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2026-03-27 23:17:05 +0530

    progress in upload data

--- frontend/src/Components/UploadData.jsx
+++ frontend/src/Components/UploadData.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import axios from 'axios'
+import { MdOutlineCancelPresentation } from "react-icons/md";
 
 function UploadData() {
 	const [isDisabled, setIsDisabled] = useState(true);
@@ -8,14 +9,23 @@ function UploadData() {
 	const [course, setCourse] = useState('')
 	const [file, setFile] = useState(null);
 	const [error, setError] = useState('');
+	const [successMsg, setSuccessMsg] = useState('');
+	const fileInputRef = useRef(null);
 
-	const handleYearChange = (e) => {
-		setAcademicYear(e.target.value);
-		academicYear !== '' ? setIsDisabled(true) : setIsDisabled(false);
+	const currentYear = new Date().getFullYear();
+	const yearList = [2024];
+	for (let year = yearList[0] + 1; year <= currentYear; year++) {
+		yearList.push(year);
+	}
+
+	const handleCourse = (e) => {
+		setCourse(e.target.value);
+		course !== '' ? setIsDisabled(true) : setIsDisabled(false);
 	}
 
 	const handleFileChange = (e) => {
 		const selectedFile = e.target.files[0];
+
 		const validTypes = [
 			"application/vnd.ms-excel", // .xls
 			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", // .xlsx
@@ -32,8 +42,21 @@ function UploadData() {
 		setFile(selectedFile);
 	}
 
+	const handleRemoveFile = () => {
+		setFile(null);
+		fileInputRef.current.value = '';
+	}
+
 	const handleUpload = async () => {
-		if (!file) return;
+		if (!subjectId || !academicYear || !course) {
+			setError("Please fill all the fields");
+			return;
+		}
+		if (!file) {
+			setError("Please choose a file!");
+			return;
+		}
+
 
 		const formData = new FormData();
 		formData.append('excelFile', file);
@@ -43,6 +66,8 @@ function UploadData() {
 
 		try {
 			const res = await axios.post('/mark/upload-raw', formData);
+			setError('');
+			setSuccessMsg(res.data.message);
 			console.log(res);
 		} catch (err) {
 			console.log("Error on handleUpload || ", err);
@@ -59,22 +84,25 @@ function UploadData() {
 				<select
 					className='border border-gray-300 rounded-sm flex-1 px-2 py-1'
 					value={academicYear}
-					onChange={handleYearChange}
+					onChange={(e) => setAcademicYear(e.target.value)}
 				>
 					<option value=''>Select a year</option>
-					<option value='2025'>2025</option>
-					<option value='value 2'>value 2</option>
-					<option value='value 3'>value 3</option>
+					{yearList.map(year => (
+						<option key={year} value={year}>
+							{year}
+						</option>
+					))}
 				</select>
 				<select
 					className='border border-gray-300 rounded-sm flex-1 px-2 py-1'
 					value={course}
-					onChange={(e) => setCourse(e.target.value)}
+					onChange={handleCourse}
 				>
 					<option value=''>Select a course</option>
 					<option value='BCA'>BCA</option>
 					<option value='MCA'>MCA</option>
 				</select>
+				{/* TODO: FETCH DATA DYNAMICALLY FOR SUBJECT ID BELOW */}
 				<select
 					className={`${isDisabled ? 'bg-gray-50 text-gray-400' : null} border border-gray-300 rounded-sm flex-1 px-2 py-1`}
 					value={subjectId}
@@ -90,9 +118,10 @@ function UploadData() {
 
 			<div className='flex gap-5 my-7'>
 				<div className='flex w-3/5 border-2 border-gray-300 rounded-sm'>
-					<label className='bg-gray-200 border-gray-300 px-3 border-r-2'>
+					<label className='bg-gray-200 border-gray-300 px-3 border-r-2 cursor-pointer'>
 						Choose File
 						<input
+							ref={fileInputRef}
 							type='file'
 							accept='.xls, .xlsx'
 							className='hidden'
@@ -102,10 +131,18 @@ function UploadData() {
 					<div className='w-2/3 mx-2'>
 						{!file ? 'No file choose' : file.name}
 					</div>
+					{file && (
+						<div
+							className='ml-auto mr-2'
+							onClick={handleRemoveFile}
+						>
+							<MdOutlineCancelPresentation className='h-full w-[25px] cursor-pointer text-red-600' />
+						</div>
+					)}
 				</div>
 				<div className='flex-1'>
 					<button
-						className='bg-blue-900 w-1/2 rounded-sm text-white p-1'
+						className='bg-blue-900 w-1/2 rounded-sm text-white p-1 cursor-pointer hover:bg-blue-800 duration-200'
 						onClick={handleUpload}
 					>
 						Upload
@@ -118,6 +155,9 @@ function UploadData() {
 						{error}
 					</p>
 				)}
+				<p className="text-green-500 text-sm ml-2">
+					{successMsg}
+				</p>
 			</div>
 		</div>
 	)
