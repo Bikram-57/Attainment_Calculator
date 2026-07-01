28f23abe99dee96e4b48b8c17f3383a936ae87ea
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2026-07-01 23:43:42 +0530

    update in color theme

--- frontend/src/constants/theme.js
+++ frontend/src/constants/theme.js
@@ -1,6 +1,7 @@
 export const COLORS = {
     // mint: '#00A19B',
-    mint: '#8b004a',
+    // mint: '#8b004a',
+    mint: '#740e3f',
     // mintDark: '#008985',
     mintDark: '#7b0042',
     latte: '#fffaf3',
