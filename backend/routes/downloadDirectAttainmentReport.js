e1e5984820eec67d4031739b3e70ebeded8e298e
Bikram-57 <bikramdas20032@gmail.com>
2026-08-21 16:11:27 +0530

    add role base access on some controllers

--- backend/routes/downloadDirectAttainmentReport.js
+++ backend/routes/downloadDirectAttainmentReport.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const verifyRoles = require('../middleware/verifyRoles');
 const {
   downloadDirectPoReport,
 } = require("../controllers/downloadDirectAttainmentReport");
 
 const router = express.Router();
 
-router.post("/direct-po", downloadDirectPoReport);
+router.post("/direct-po", verifyRoles('admin'), downloadDirectPoReport);
 // router.get('/direct-po', downloadDirectPoReport);
 
 module.exports = router;
 
-// must add route security through jwt
